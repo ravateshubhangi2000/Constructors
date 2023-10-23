97f1b131bcc65b487ab244cd0d9fb810b65583dd
[shubhangi] <[ravateshubhangi@gmail.com]>
2023-10-23 20:06:35 +0530

    changes have been committed

---
+++ src/constructors/ParameterizedConst.java
@@ -0,0 +1,26 @@
+package constructors;
+class Student{
+	int id;
+	String name;
+	Student(int i,String n)
+	{
+		id=i;
+		name=n;
+	}
+	void display() 
+	{
+		System.out.println(id+" "+name);
+	}
+}
+
+public class ParameterizedConst 
+{
+	public static void main(String[] args) 
+	{
+		Student std1=new Student(2,"Alex");
+		Student std2=new Student(10,"Annie");
+		std1.display();
+		std2.display();
+	}
+
+}
